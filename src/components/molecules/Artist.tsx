5d87e133edc7125dd676246d50181c46bc0b6a1d
cristi <cristi@draghici.net>
2024-05-01 00:12:06 +0300

    Refactor events loading for the current artist

--- src/components/molecules/Artist.tsx
+++ src/components/molecules/Artist.tsx
@@ -1,62 +1,38 @@
 import ArtistDetails from "@/components/atoms/ArtistDetails";
-import EventDetails from "@/components/atoms/EventDetails";
+import ArtistEvents from "./ArtistEvents";
 
 import useGlobalContext from "@/hooks/useGlobalContext";
 
 import useGetArtists from "@/api/useGetArtists";
-import useGetArtistsEvents from "@/api/useGetArtistsEvents";
-
 import type { Artist } from "@/types";
 
 const Artist = () => {
-  const { artistName, selectedArtist, handleSelectArtistAndEvent } =
-    useGlobalContext();
+  const { artistName, selectedArtist } = useGlobalContext();
 
   const {
     artist,
     isLoading: isLoadingArtist,
-    error: errorArtist,
+    error,
   } = useGetArtists(artistName);
 
-  const {
-    events,
-    isLoading: isLoadingEvents,
-    // error: errorEvents,
-  } = useGetArtistsEvents(artist ? artistName : undefined);
-
-  if (isLoadingArtist || isLoadingEvents) {
-    return <div>Loading...</div>;
+  if (isLoadingArtist) {
+    return <div>Loading artist...</div>;
   }
 
-  if (errorArtist) {
+  if (error) {
     return <div>Error fetching the artist...</div>;
   }
 
-  if (!artistName || !artist) {
-    if (selectedArtist) {
-      return <ArtistDetails artist={selectedArtist} />;
-    }
+  const currentArtist = artist || selectedArtist;
 
+  if (!currentArtist) {
     return <div></div>;
   }
 
   return (
     <>
-      <ArtistDetails artist={artist} />
-
-      {events.length > 0 &&
-        events.map((event) => (
-          <EventDetails
-            key={event.id}
-            event={event}
-            isVenueVisible={false}
-            isOffersVisible={false}
-            onClick={(e) => {
-              e.preventDefault();
-              handleSelectArtistAndEvent(artist, event);
-            }}
-          />
-        ))}
+      <ArtistDetails artist={currentArtist} />
+      <ArtistEvents artist={currentArtist} />
     </>
   );
 };
---
+++ src/components/molecules/ArtistEvents.tsx
@@ -0,0 +1,52 @@
+import { ComponentProps } from "react";
+import EventDetails from "@/components/atoms/EventDetails";
+
+import useGlobalContext from "@/hooks/useGlobalContext";
+import useGetArtistsEvents from "@/api/useGetArtistsEvents";
+
+import type { Artist } from "@/types";
+
+interface ArtistEventProps extends ComponentProps<"section"> {
+  artist: Artist;
+}
+
+const ArtistEvents: React.FunctionComponent<ArtistEventProps> = ({
+  artist,
+  ...rest
+}) => {
+  const { handleSelectArtistAndEvent } = useGlobalContext();
+
+  const {
+    events,
+    isLoading: isLoadingEvents,
+    error,
+  } = useGetArtistsEvents(artist.name);
+
+  if (isLoadingEvents) {
+    return <div>Loading events...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching the events...</div>;
+  }
+
+  return (
+    <section {...rest}>
+      {events.length > 0 &&
+        events.map((event) => (
+          <EventDetails
+            key={event.id}
+            event={event}
+            isVenueVisible={false}
+            isOffersVisible={false}
+            onClick={(e) => {
+              e.preventDefault();
+              handleSelectArtistAndEvent(artist, event);
+            }}
+          />
+        ))}
+    </section>
+  );
+};
+
+export default ArtistEvents;
