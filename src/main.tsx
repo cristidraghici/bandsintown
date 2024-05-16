cf55f3994e4c4bfa00965a7a0af2cfd9786a5e3c
cristi <cristi@draghici.net>
2024-05-16 14:57:08 +0300

    Simplify the file structure inside the ./api folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SWRConfig } from "swr";
 
-import SWRConfigProvider from "@/api/components/SWRConfigProvider.tsx";
 import GlobalProvider from "@/contexts/GlobalContext.tsx";
+import fetcher from "@/utils/fetcher";
 
 import App from "./App.tsx";
 
@@ -10,10 +11,19 @@ import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SWRConfigProvider>
-      <GlobalProvider>
+    <GlobalProvider>
+      <SWRConfig
+        value={{
+          fetcher,
+          provider: () => new Map(),
+          refreshInterval: 100000,
+          revalidateOnFocus: false,
+          revalidateOnReconnect: false,
+          dedupingInterval: 10000,
+        }}
+      >
         <App />
-      </GlobalProvider>
-    </SWRConfigProvider>
+      </SWRConfig>
+    </GlobalProvider>
   </React.StrictMode>
 );
